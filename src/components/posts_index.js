8412f786ff4b7f31a71b4d936018721c241a6c50
Cody Condon <codycondon50@gmail.com>
2016-03-29 17:41:55 -0400

    add redux form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsIndex extends Component {
   componentWillMount(){
-    console.log('this would be a good time to call an action creator to fetch posts');
+    this.props.fetchPosts();
   }
   render() {
     return (
-      <div>List of blog posts</div>
-    )
+      <div>
+
+      <div className="text-xs-right">
+        <Link to="posts/new" className="btn btn-primary">Add a Post</Link>
+      </div>
+      List of blog posts
+      </div>
+    );
   }
 }
 
-export default PostsIndex;
+
+export default connect(null, { fetchPosts })(PostsIndex);
---
+++ src/components/posts_new.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+  render() {
+    return(
+    <div>Create Form</div>
+  );
+  }
+}
+
+export default PostsNew;
