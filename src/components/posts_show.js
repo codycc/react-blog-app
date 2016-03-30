d7c4b2c41e7379232777580d955739da7f2177fe
Cody Condon <codycondon50@gmail.com>
2016-03-29 22:53:04 -0400

    give use ability to delete post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchPost} from '../actions/index';
+import {fetchPost, deletePost} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsShow extends Component {
   componentWillMount(){
     this.props.fetchPost(this.props.params.id)
+  }
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id)
   }
   render() {
     const {post} = this.props;
@@ -14,9 +18,14 @@ class PostsShow extends Component {
     }
     return (
       <div>
-        <h3>{post.title}</h3>
-        <h6>Categories: {post.categories}</h6>
-        <p>{post.content}</p>
+        <Link to="/">Back to Index</Link>
+        <button
+        className="btn btn-danger pull-xs-right"
+        onClick={this.onDeleteClick.bind(this)}>
+        Delete Post</button>
+          <h3>{post.title}</h3>
+          <h6>Categories: {post.categories}</h6>
+          <p>{post.content}</p>
       </div>
     );
 
@@ -27,4 +36,4 @@ class PostsShow extends Component {
 function mapStateToProps(state) {
   return {post: state.posts.post};
 }
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
